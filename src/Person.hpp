07709825a4603aaf47ef750e87bfab46835587e8
Julien Marrec <julien.marrec@gmail.com>
2021-08-31 00:05:41 +0200

    Some changes

--- src/Person.hpp
+++ src/Person.hpp
@@ -1,23 +1,22 @@
 #ifndef PERSON_HPP
 #define PERSON_HPP
 
-#include <string>
-#include <vector>
+#include <array>
 #include <optional>
+#include <string>
+#include <string_view>
 
 namespace civil_registry {
 
 class Person
 {
  public:
-  inline static std::vector<std::string> rejectedNames = {"Adolf"};
+  inline constexpr static std::array<std::string_view, 1> rejectedNames = {"Adolf"};
 
   Person(std::string t_name) noexcept;
 
-  std::string getName() const;
+  [[nodiscard]] std::string getName() const;
   bool setName(const std::string& t_newName);
-
-  unsigned getIndex() const;
 
  private:
   std::string m_name;
